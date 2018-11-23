aaa62c8fb73ae8e28587b372b6a3bf1cef11cbe5
Vladimir <vlabum@gmail.com>
2018-11-23 22:42:17 +0500

    Initial commit

---
+++ desktop/src/ru/vlabum/android/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.vlabum.android.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.vlabum.android.games.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
